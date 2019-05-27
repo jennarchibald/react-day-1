3da72342fab2774a34ce17d2999c6806cd9ff805
jennarchibald <jennarchibald91@gmail.com>
2019-05-27 20:18:41 +0100

    filmlist and film

---
+++ src/components/FilmList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Film from './Film';
+
+class FilmList extends Component{
+  render(){
+    const filmNodes = this.props.data.map((film) => {
+      return (
+        <Film name = {film.name} url = {film.url} key = {film.id}>{film.text}</Film>
+      );
+    });
+
+    return(
+      <ul className = "film-list">
+        {filmNodes}
+      </ul>
+    );
+  }
+}
+
+export default FilmList;
