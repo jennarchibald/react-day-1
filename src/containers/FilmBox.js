98c5ae49d2a4aa3074e7019cffa3965782d83c2a
jennarchibald <jennarchibald91@gmail.com>
2019-05-27 20:07:54 +0100

    inital commit and filmbox

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilmBox extends Component {
+  render(){
+    return (
+      <div className = "film-box">
+      <h1> Upcoming Film Releases For the UK </h1>
+      </div>
+    )
+  }
+}
+
+export default FilmBox;
